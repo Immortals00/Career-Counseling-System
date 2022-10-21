fee9e9aaede0acabbda80a273a02b74cb29ba9c3
Mitra1Foundation <92683193+Mitra1Foundation@users.noreply.github.com>
2022-10-21 21:53:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
 import Checkout from "./Checkout";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={<> <Header /> <Home /> </>} />
-          <Route path="/login" element={<> <Header /> <Login /> </>} />
-          <Route path="/checkout" element={<> <Header /> <Checkout /> </>} />
+          <Route path="/" component ={<> <Header /> <Home /> </>} />
+          <Route path="/login" component ={<> <Header /> <Login /> </>} />
+          <Route path="/checkout" component ={<> <Header /> <Checkout /> </>} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
